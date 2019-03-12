e555c7a46080d853d81eeb1e89ed2e846e53e75a
Heiner Wolf <wolf.heiner@gmail.com>
2019-03-12 18:02:21 +0100

    ClusterId default to Development

--- src/Wolfspelz.OrleansSample.Shared/Settings.cs
+++ src/Wolfspelz.OrleansSample.Shared/Settings.cs
@@ -4,7 +4,7 @@ namespace Wolfspelz.OrleansSample.Shared
 {
     public class Settings
     {
-        public static string ClusterId { get; set; } = "Demo";
+        public static string ClusterId { get; set; } = "Development";
         public static string ServiceId { get; set; } = "Sample";
         public static string SmsProviderName { get; set; } = "SMSProvider";
     }
